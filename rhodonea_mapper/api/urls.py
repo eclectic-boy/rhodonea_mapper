4f889b0216813d0f3a467a7e70f5a31ecbab5c83
eclectic-boy <eclectic-boy@users.noreply.github.com>
2020-02-05 17:49:44 +0000

    Feature/clean api and add swagger docs (#7)
    * Add swagger docs
    
    * clean up

--- rhodonea_mapper/api/urls.py
+++ rhodonea_mapper/api/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from rhodonea_mapper.api.hello_world import HelloWorld
 from rhodonea_mapper.api.layers import LayersViewSet
 
 
@@ -12,5 +11,4 @@ router.register('layers', LayersViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('hello-world/', HelloWorld.as_view(), name='api-hello-world'),
 ]
